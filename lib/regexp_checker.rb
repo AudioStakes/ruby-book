49e5bc67bd35098998e1883ee0b7bfb824d055eb
AudioStakes <audiostakes@gmail.com>
2019-10-13 22:20:36 +0900

    例外処理を組み込んだ

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -1,10 +1,16 @@
 print 'Text?:'
 text = gets.chomp
-# 動作確認のため変数の中身を表示
-print 'Pattern?:'
-pattern = gets.chomp
 
-regexp = Regexp.new(pattern)
+# 例外処理を組み込んで再入力可能にする
+begin
+    print 'Pattern?:'
+    pattern = gets.chomp
+    regexp = Regexp.new(pattern)
+rescue RegexpError => e
+    puts "Invalid pattern: #{e.message}"
+    retry
+end
+
 matches = text.scan(regexp)
 if matches.size > 0
     puts "Matched: #{matches.join(',')}"
