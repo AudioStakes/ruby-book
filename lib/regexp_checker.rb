06834a412a0d909a8a3b540b72b1b0742941aadf
AudioStakes <daisuke.sato@enishi-tech.com>
2021-07-02 15:40:40 +0900

    rubocop -A

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 print 'Text?:'
 text = gets.chomp
 
 # 例外処理を組み込んで再入力可能にする
 begin
-    print 'Pattern?:'
-    pattern = gets.chomp
-    regexp = Regexp.new(pattern)
+  print 'Pattern?:'
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
 rescue RegexpError => e
-    puts "Invalid pattern: #{e.message}"
-    retry
+  puts "Invalid pattern: #{e.message}"
+  retry
 end
 
 matches = text.scan(regexp)
-if matches.size > 0
-    puts "Matched: #{matches.join(',')}"
+if matches.size.positive?
+  puts "Matched: #{matches.join(',')}"
 else
-    puts "Nothing matched"
+  puts 'Nothing matched'
 end
